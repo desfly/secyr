aef406d6cfcc6630b7982f1e90e0fba52be8ec6e
desfly <155248075+desfly@users.noreply.github.com>
2026-08-06 00:25:17 +0300

    Update SDMMC mock with CSD sector size

--- tests/esp-idf-mock/include/sdmmc_cmd.h
+++ tests/esp-idf-mock/include/sdmmc_cmd.h
@@ -1,7 +1,23 @@
 #pragma once
 #include "esp_err.h"
-struct sdmmc_card_t {};
-struct sdmmc_host_t { int slot; };
+
+struct sdmmc_csd_t {
+    unsigned sector_size;
+};
+
+struct sdmmc_card_t {
+    sdmmc_csd_t csd;
+};
+
+struct sdmmc_host_t {
+    int slot;
+};
+
 #define SDSPI_HOST_DEFAULT() sdmmc_host_t{}
-struct sdspi_device_config_t { int gpio_cs; int host_id; };
+
+struct sdspi_device_config_t {
+    int gpio_cs;
+    int host_id;
+};
+
 #define SDSPI_DEVICE_CONFIG_DEFAULT() sdspi_device_config_t{}
