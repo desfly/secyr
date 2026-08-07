a9d3a549ae64df38aeaa4a260d229a75b87e372b
desfly <155248075+desfly@users.noreply.github.com>
2026-08-07 22:37:50 +0300

    Bump Android to Build-0033

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "ua.homeguard.s3"
         minSdk = 26
         targetSdk = 35
-        versionCode = 32
-        versionName = "0.0.32"
+        versionCode = 33
+        versionName = "0.0.33"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
