b99880131799970333ad2cc8ef02b2a91c9f9b77
desfly <155248075+desfly@users.noreply.github.com>
2026-08-07 21:51:19 +0300

    Set Android version to 0.0.32

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "ua.homeguard.s3"
         minSdk = 26
         targetSdk = 35
-        versionCode = 31
-        versionName = "0.0.31"
+        versionCode = 32
+        versionName = "0.0.32"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
