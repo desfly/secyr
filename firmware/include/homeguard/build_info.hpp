eb11d725b56a91623dfc912cbe02e85f83e50638
desfly <155248075+desfly@users.noreply.github.com>
2026-08-07 21:05:37 +0300

    Start HomeGuard-S3 Build-0030

--- firmware/include/homeguard/build_info.hpp
+++ firmware/include/homeguard/build_info.hpp
@@ -1,7 +1,7 @@
 #pragma once
 #include <string_view>
 namespace hg::build {
-inline constexpr std::string_view number = "0013";
-inline constexpr std::string_view version = "0.0.13";
-inline constexpr std::string_view label = "HomeGuard-S3 Build-0013";
+inline constexpr std::string_view number = "0030";
+inline constexpr std::string_view version = "0.0.30";
+inline constexpr std::string_view label = "HomeGuard-S3 Build-0030";
 }
