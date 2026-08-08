80f6c7be5f50f3abbb53129fb8e04109f3094c83
desfly <155248075+desfly@users.noreply.github.com>
2026-08-08 19:48:39 +0300

    Build-0058: add STA connect state to WiFi runtime

--- firmware/esp-idf/main/hg_wifi_provisioning.hpp
+++ firmware/esp-idf/main/hg_wifi_provisioning.hpp
@@ -10,8 +10,10 @@ namespace homeguard::idf {
 class WifiProvisioningRuntime {
 public:
     esp_err_t start(bool provisioning_required);
+    esp_err_t connect_station(const char* ssid, const char* password);
 
     [[nodiscard]] bool started() const { return started_; }
+    [[nodiscard]] bool station_connecting() const { return station_connecting_; }
     [[nodiscard]] const char* ssid() const { return ssid_.data(); }
     [[nodiscard]] const char* ip_address() const { return ip_address_.data(); }
 
@@ -19,6 +21,7 @@ private:
     std::array<char, 33> ssid_{};
     std::array<char, 16> ip_address_{};
     bool started_{};
+    bool station_connecting_{};
 };
 
 }  // namespace homeguard::idf
