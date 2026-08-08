a87bc73c93cdb144063dec516b3cf27f3736892b
desfly <155248075+desfly@users.noreply.github.com>
2026-08-08 19:51:28 +0300

    Build-0058: synchronize runtime build label

--- firmware/include/homeguard/build_info.hpp
+++ firmware/include/homeguard/build_info.hpp
@@ -1,7 +1,7 @@
 #pragma once
 #include <string_view>
 namespace hg::build {
-inline constexpr std::string_view number = "0057";
-inline constexpr std::string_view version = "0.0.57";
-inline constexpr std::string_view label = "HomeGuard-S3 Build-0057";
+inline constexpr std::string_view number = "0058";
+inline constexpr std::string_view version = "0.0.58";
+inline constexpr std::string_view label = "HomeGuard-S3 Build-0058";
 }
