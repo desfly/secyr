c99a69364d2515d6403907a7f5028e1214f09396
desfly <155248075+desfly@users.noreply.github.com>
2026-08-08 16:08:34 +0300

    Build-0051: bump Android version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "ua.homeguard.s3"
         minSdk = 26
         targetSdk = 35
-        versionCode = 50
-        versionName = "0.0.50"
+        versionCode = 51
+        versionName = "0.0.51"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
