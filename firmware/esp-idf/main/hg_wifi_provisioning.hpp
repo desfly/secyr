240ebbee60b3807990b95a7586f5c8dd3115f257
desfly <155248075+desfly@users.noreply.github.com>
2026-08-08 22:47:15 +0300

    Build-0058: track real STA connection state and IP

--- firmware/esp-idf/main/hg_wifi_provisioning.hpp
+++ firmware/esp-idf/main/hg_wifi_provisioning.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "esp_err.h"
+#include "esp_event.h"
 
 #include <array>
 #include <cstdint>
@@ -14,14 +15,23 @@ public:
 
     [[nodiscard]] bool started() const { return started_; }
     [[nodiscard]] bool station_connecting() const { return station_connecting_; }
+    [[nodiscard]] bool station_connected() const { return station_connected_; }
     [[nodiscard]] const char* ssid() const { return ssid_.data(); }
     [[nodiscard]] const char* ip_address() const { return ip_address_.data(); }
+    [[nodiscard]] const char* station_ip_address() const { return station_ip_address_.data(); }
 
 private:
+    static void network_event_handler(void* arg,
+                                      esp_event_base_t event_base,
+                                      std::int32_t event_id,
+                                      void* event_data);
+
     std::array<char, 33> ssid_{};
     std::array<char, 16> ip_address_{};
+    std::array<char, 16> station_ip_address_{};
     bool started_{};
     bool station_connecting_{};
+    bool station_connected_{};
 };
 
 }  // namespace homeguard::idf
