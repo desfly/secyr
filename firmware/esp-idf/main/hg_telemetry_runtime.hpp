977d637b9be5220aed1ccc63518b5b478e15a69f
desfly <155248075+desfly@users.noreply.github.com>
2026-08-09 11:15:38 +0300

    Build-0060: publish system telemetry to cloud

--- firmware/esp-idf/main/hg_telemetry_runtime.hpp
+++ firmware/esp-idf/main/hg_telemetry_runtime.hpp
@@ -2,19 +2,25 @@
 
 #include "esp_err.h"
 
+namespace hg { class SystemModel; }
 namespace homeguard::idf {
 
+class CloudLink;
 class HardwareBootstrap;
 
 class TelemetryRuntime {
 public:
-    esp_err_t start(HardwareBootstrap* hardware);
+    esp_err_t start(HardwareBootstrap* hardware, hg::SystemModel* model, CloudLink* cloud);
+    esp_err_t publish_now();
 
 private:
     static void task_entry(void* context);
     void run();
 
     HardwareBootstrap* hardware_{nullptr};
+    hg::SystemModel* model_{nullptr};
+    CloudLink* cloud_{nullptr};
+    unsigned long long sequence_{0};
 };
 
 }  // namespace homeguard::idf
