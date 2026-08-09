befe74f860b62f13161f5244308921940e394051
desfly <155248075+desfly@users.noreply.github.com>
2026-08-09 09:38:41 +0300

    wire cloud command callback

--- firmware/esp-idf/main/hg_cloud_link.hpp
+++ firmware/esp-idf/main/hg_cloud_link.hpp
@@ -4,15 +4,23 @@
 #include "mqtt_client.h"
 
 #include <array>
+#include <cstddef>
 #include <cstdint>
 
 namespace homeguard::idf {
 
 class CloudLink {
 public:
+    using CommandHandler = void (*)(const char* payload, std::size_t length, void* context);
+
     esp_err_t prepare_identity();
     esp_err_t start(const char* broker_uri, const char* username, const char* password);
     void stop();
+    void set_command_handler(CommandHandler handler, void* context = nullptr)
+    {
+        command_handler_ = handler;
+        command_context_ = context;
+    }
 
     [[nodiscard]] const char* device_id() const { return device_id_.data(); }
     [[nodiscard]] bool configured() const { return configured_; }
@@ -22,6 +30,7 @@ public:
     [[nodiscard]] std::uint32_t command_count() const { return command_count_; }
 
     esp_err_t publish_state(const char* json, int qos = 1, bool retain = true);
+    esp_err_t publish_command_response(const char* json);
 
 private:
     static void mqtt_event_handler(void* handler_args,
@@ -32,7 +41,6 @@ private:
     void make_device_id();
     void make_topics();
     void publish_online(bool online);
-    void publish_command_response(const char* json);
 
     esp_mqtt_client_handle_t client_{};
     std::array<char, 32> device_id_{};
@@ -40,6 +48,8 @@ private:
     std::array<char, 96> availability_topic_{};
     std::array<char, 96> command_topic_{};
     std::array<char, 96> response_topic_{};
+    CommandHandler command_handler_{};
+    void* command_context_{};
     bool configured_{};
     bool connected_{};
     std::uint32_t connect_count_{};
