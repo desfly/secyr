50e4090afc81fb42e97296f157c848264493c70f
desfly <155248075+desfly@users.noreply.github.com>
2026-08-12 09:43:04 +0300

    Add Android access role and capability model

---
+++ android/app/src/main/java/ua/homeguard/s3/model/AccessModels.kt
@@ -0,0 +1,31 @@
+package ua.homeguard.s3.model
+
+enum class AccessRole { ADMIN, USER, GUEST }
+
+data class AccessCapabilities(
+    val monitor: Boolean = true,
+    val armHome: Boolean = false,
+    val armAway: Boolean = false,
+    val disarm: Boolean = false,
+    val panic: Boolean = false,
+    val valves: Boolean = false,
+    val networkConfigure: Boolean = false,
+    val accessManage: Boolean = false,
+    val serviceInvalidate: Boolean = false,
+) {
+    fun allows(command: CommandType): Boolean = when (command) {
+        CommandType.ARM_HOME -> armHome
+        CommandType.ARM_AWAY -> armAway
+        CommandType.DISARM -> disarm
+        CommandType.OPEN_VALVES, CommandType.CLOSE_VALVES -> valves
+        CommandType.SILENCE, CommandType.RESET_ALARM,
+        CommandType.ENTER_MAINTENANCE, CommandType.EXIT_MAINTENANCE -> false
+    }
+}
+
+data class AccessSession(
+    val actor: String,
+    val name: String,
+    val role: AccessRole,
+    val capabilities: AccessCapabilities,
+)
