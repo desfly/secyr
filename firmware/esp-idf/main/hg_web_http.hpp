c03af50dffad8ed8c30401dea6e334bab026316b
desfly <155248075+desfly@users.noreply.github.com>
2026-08-12 08:32:55 +0300

    Serve embedded Bruce JPEG from WebHttp

--- firmware/esp-idf/main/hg_web_http.hpp
+++ firmware/esp-idf/main/hg_web_http.hpp
@@ -13,6 +13,7 @@ private:
     static esp_err_t index_get(httpd_req_t* request);
     static esp_err_t css_get(httpd_req_t* request);
     static esp_err_t js_get(httpd_req_t* request);
+    static esp_err_t bruce_get(httpd_req_t* request);
     static esp_err_t send_asset(httpd_req_t* request,
                                 const char* content_type,
                                 const unsigned char* start,
