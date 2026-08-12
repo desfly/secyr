9497382f9a916e4ec8d429c13c4c6218cf1d3d80
desfly <155248075+desfly@users.noreply.github.com>
2026-08-12 17:01:51 +0300

    Add admin-managed persisted cloud MQTT configuration

--- firmware/esp-idf/main/hg_cloud_http.hpp
+++ firmware/esp-idf/main/hg_cloud_http.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "hg_cloud_nvs.hpp"
+#include "homeguard/access_control.hpp"
 #include "esp_err.h"
 #include "esp_http_server.h"
 
@@ -9,11 +11,19 @@ class CloudLink;
 
 class CloudHttp {
 public:
-    esp_err_t register_handlers(httpd_handle_t server, CloudLink* cloud);
+    esp_err_t initialize(CloudLink* cloud, homeguard::AccessControl* access);
+    esp_err_t register_handlers(httpd_handle_t server);
 
 private:
     static esp_err_t status_get(httpd_req_t* request);
+    static esp_err_t config_post(httpd_req_t* request);
+    esp_err_t handle_config(httpd_req_t* request);
+    esp_err_t apply(const CloudConfigRecord& config, bool persist);
+
     CloudLink* cloud_{};
+    homeguard::AccessControl* access_{};
+    CloudNvsStore store_{};
+    CloudConfigRecord config_{};
 };
 
 }  // namespace homeguard::idf
