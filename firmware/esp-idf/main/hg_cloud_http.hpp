5656818ca1e1dcd01f700dd0021af91b74d8f497
desfly <155248075+desfly@users.noreply.github.com>
2026-08-12 18:29:22 +0300

    cloud: expose admin configuration endpoint

--- firmware/esp-idf/main/hg_cloud_http.hpp
+++ firmware/esp-idf/main/hg_cloud_http.hpp
@@ -3,17 +3,29 @@
 #include "esp_err.h"
 #include "esp_http_server.h"
 
+namespace homeguard { class AccessControl; }
+
 namespace homeguard::idf {
 
 class CloudLink;
+class CloudNvsStore;
 
 class CloudHttp {
 public:
-    esp_err_t register_handlers(httpd_handle_t server, CloudLink* cloud);
+    esp_err_t register_handlers(
+        httpd_handle_t server,
+        CloudLink* cloud,
+        CloudNvsStore* store,
+        homeguard::AccessControl* access_control);
 
 private:
     static esp_err_t status_get(httpd_req_t* request);
+    static esp_err_t config_post(httpd_req_t* request);
+    esp_err_t handle_config(httpd_req_t* request);
+
     CloudLink* cloud_{};
+    CloudNvsStore* store_{};
+    homeguard::AccessControl* access_control_{};
 };
 
 }  // namespace homeguard::idf
