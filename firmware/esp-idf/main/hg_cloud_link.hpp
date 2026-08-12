dd5e426800dda804983f63138ec1ef1364b5e36c
desfly <155248075+desfly@users.noreply.github.com>
2026-08-12 18:32:34 +0300

    cloud: attach MQTT commands to live system runtime

--- firmware/esp-idf/main/hg_cloud_link.hpp
+++ firmware/esp-idf/main/hg_cloud_link.hpp
@@ -6,6 +6,9 @@
 #include <array>
 #include <cstdint>
 
+namespace homeguard { class AccessControl; }
+namespace hg { class SystemModel; class SystemEventBus; }
+
 namespace homeguard::idf {
 
 class CloudLink {
@@ -13,6 +16,7 @@ public:
     esp_err_t prepare_identity();
     esp_err_t start(const char* broker_uri, const char* username, const char* password);
     void stop();
+    void set_command_runtime(hg::SystemModel* model, hg::SystemEventBus* bus, homeguard::AccessControl* access_control);
 
     [[nodiscard]] const char* device_id() const { return device_id_.data(); }
     [[nodiscard]] bool configured() const { return configured_; }
@@ -31,12 +35,17 @@ private:
     void make_device_id();
     void make_topics();
     void publish_online(bool online);
+    void handle_command(const char* data, std::size_t size);
 
     esp_mqtt_client_handle_t client_{};
     std::array<char, 32> device_id_{};
     std::array<char, 96> state_topic_{};
     std::array<char, 96> availability_topic_{};
     std::array<char, 96> command_topic_{};
+    std::array<char, 96> response_topic_{};
+    hg::SystemModel* model_{};
+    hg::SystemEventBus* bus_{};
+    homeguard::AccessControl* access_control_{};
     bool configured_{};
     bool connected_{};
     std::uint32_t connect_count_{};
