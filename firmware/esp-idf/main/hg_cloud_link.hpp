8c7c618851de0555ee00d3df15713f379f80aab5
desfly <155248075+desfly@users.noreply.github.com>
2026-08-12 16:38:22 +0300

    Route cloud commands through runtime callback

--- firmware/esp-idf/main/hg_cloud_link.hpp
+++ firmware/esp-idf/main/hg_cloud_link.hpp
@@ -4,15 +4,24 @@
 #include "mqtt_client.h"
 
 #include <array>
+#include <cstddef>
 #include <cstdint>
+#include <string>
 
 namespace homeguard::idf {
 
 class CloudLink {
 public:
+    using CommandHandler = std::string (*)(const char* payload, std::size_t size, void* context);
+
     esp_err_t prepare_identity();
     esp_err_t start(const char* broker_uri, const char* username, const char* password);
     void stop();
+
+    void set_command_handler(CommandHandler handler, void* context) {
+        command_handler_ = handler;
+        command_context_ = context;
+    }
 
     [[nodiscard]] const char* device_id() const { return device_id_.data(); }
     [[nodiscard]] bool configured() const { return configured_; }
@@ -31,12 +40,16 @@ private:
     void make_device_id();
     void make_topics();
     void publish_online(bool online);
+    void publish_command_response(const std::string& response);
 
     esp_mqtt_client_handle_t client_{};
     std::array<char, 32> device_id_{};
     std::array<char, 96> state_topic_{};
     std::array<char, 96> availability_topic_{};
     std::array<char, 96> command_topic_{};
+    std::array<char, 96> response_topic_{};
+    CommandHandler command_handler_{};
+    void* command_context_{};
     bool configured_{};
     bool connected_{};
     std::uint32_t connect_count_{};
