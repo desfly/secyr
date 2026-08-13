f34627da4dfe9fe8f8635be7fe0b5af586fe5533
desfly <155248075+desfly@users.noreply.github.com>
2026-08-13 14:00:16 +0300

    Fix Android JSON unit tests

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -58,4 +58,5 @@ dependencies {
     implementation("com.journeyapps:zxing-android-embedded:4.3.0")
 
     testImplementation("junit:junit:4.13.2")
+    testImplementation("org.json:json:20250517")
 }
