549f2ce3c9ba8603170e864fb9d161ef03f416e7
desfly <155248075+desfly@users.noreply.github.com>
2026-08-14 10:57:19 +0300

    android: expose input polarity operations

--- android/app/src/main/java/ua/homeguard/s3/network/DeviceApi.kt
+++ android/app/src/main/java/ua/homeguard/s3/network/DeviceApi.kt
@@ -1,3 +1,10 @@
 package ua.homeguard.s3.network
 import ua.homeguard.s3.model.*
-interface DeviceApi { suspend fun command(command:DeviceCommand):CommandReply; suspend fun diagnostics():Diagnostics; suspend fun snapshot():SystemSnapshot; suspend fun challenge(type:CommandType):Long }
+interface DeviceApi {
+    suspend fun command(command: DeviceCommand): CommandReply
+    suspend fun diagnostics(): Diagnostics
+    suspend fun snapshot(): SystemSnapshot
+    suspend fun challenge(type: CommandType): Long
+    suspend fun inputPolarity(): InputPolarityConfig
+    suspend fun configureInputPolarity(config: InputPolarityConfig, actor: String, credential: String): InputPolarityConfig
+}
