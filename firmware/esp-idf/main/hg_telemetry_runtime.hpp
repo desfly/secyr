d228c81a3fb8c1d2efc2c26fba513af7e6476be9
desfly <155248075+desfly@users.noreply.github.com>
2026-08-14 12:12:34 +0300

    feat(firmware): publish real hardware telemetry frames

--- firmware/esp-idf/main/hg_telemetry_runtime.hpp
+++ firmware/esp-idf/main/hg_telemetry_runtime.hpp
@@ -1,6 +1,13 @@
 #pragma once
 
+#include "homeguard/telemetry.hpp"
 #include "esp_err.h"
+
+namespace hg {
+class SystemModel;
+}
+
+class WebsocketTelemetry;
 
 namespace homeguard::idf {
 
@@ -8,13 +15,20 @@ class HardwareBootstrap;
 
 class TelemetryRuntime {
 public:
-    esp_err_t start(HardwareBootstrap* hardware);
+    esp_err_t start(
+        HardwareBootstrap* hardware,
+        WebsocketTelemetry* websocket,
+        const hg::SystemModel* system_model);
 
 private:
     static void task_entry(void* context);
     void run();
 
     HardwareBootstrap* hardware_{nullptr};
+    WebsocketTelemetry* websocket_{nullptr};
+    const hg::SystemModel* system_model_{nullptr};
+    hg::TelemetryBuilder builder_{};
+    hg::HealthMonitor health_{};
 };
 
 }  // namespace homeguard::idf
