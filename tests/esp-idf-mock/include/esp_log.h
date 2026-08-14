0bf05990aa16485a99967c897dd68ac039af8ca0
desfly <155248075+desfly@users.noreply.github.com>
2026-08-14 12:30:21 +0300

    fix(ci): mock ESP_LOGD for telemetry syntax check

--- tests/esp-idf-mock/include/esp_log.h
+++ tests/esp-idf-mock/include/esp_log.h
@@ -1,4 +1,5 @@
 #pragma once
+#define ESP_LOGD(tag, fmt, ...) do {} while(0)
 #define ESP_LOGI(tag, fmt, ...) do {} while(0)
 #define ESP_LOGE(tag, fmt, ...) do {} while(0)
 #define ESP_LOGW(tag, fmt, ...) do {} while(0)
