7b211acfd7d1422d7a09fee9165f25b061c73e26
desfly <155248075+desfly@users.noreply.github.com>
2026-08-14 10:20:01 +0300

    firmware: add configurable input polarity model

--- firmware/esp-idf/main/hg_input_runtime.hpp
+++ firmware/esp-idf/main/hg_input_runtime.hpp
@@ -3,17 +3,43 @@
 #include "homeguard/system_model.hpp"
 #include "esp_err.h"
 
+#include <array>
+#include <cstdint>
+
 namespace homeguard::idf {
+
+enum class InputPolarity : std::uint8_t {
+    Unknown = 0,
+    ActiveHigh = 1,
+    ActiveLow = 2,
+};
+
+struct InputPolarityConfig {
+    InputPolarity tamper{InputPolarity::Unknown};
+    InputPolarity power_fail{InputPolarity::Unknown};
+};
 
 class InputRuntime {
 public:
     esp_err_t start(hg::SystemEventBus* bus);
+    void set_polarity(InputPolarityConfig config) { polarity_ = config; }
+    [[nodiscard]] InputPolarityConfig polarity() const { return polarity_; }
 
 private:
     static void task_entry(void* context);
     void run();
 
     hg::SystemEventBus* bus_{};
+    InputPolarityConfig polarity_{};
 };
+
+[[nodiscard]] constexpr bool input_is_active(InputPolarity polarity, bool raw_high)
+{
+    switch (polarity) {
+        case InputPolarity::ActiveHigh: return raw_high;
+        case InputPolarity::ActiveLow: return !raw_high;
+        default: return false;
+    }
+}
 
 }  // namespace homeguard::idf
