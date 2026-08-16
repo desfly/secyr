0e867559bbb88c1d1fd3aa942c2d0ab4218f1f77
desfly <155248075+desfly@users.noreply.github.com>
2026-08-16 11:25:59 +0300

    firmware: define safe commissioning service API

--- firmware/esp-idf/main/hg_service_http.hpp
+++ firmware/esp-idf/main/hg_service_http.hpp
@@ -9,10 +9,13 @@
 #include "homeguard/system_model.hpp"
 #include "esp_http_server.h"
 
+#include <cstdint>
 #include <mutex>
 #include <string>
 
 namespace homeguard::idf {
+
+class HardwareBootstrap;
 
 class ServiceHttp {
 public:
@@ -24,6 +27,8 @@ public:
         hg::BootReadinessReport* readiness,
         hg::PhysicalOutputRuntime* physical_outputs,
         hg::SystemEventBus* bus,
+        hg::SystemModel* model,
+        HardwareBootstrap* hardware_runtime,
         std::mutex* control_state_mutex);
 
     void set_access_control(homeguard::AccessControl* access_control) {
@@ -32,9 +37,18 @@ public:
 
 private:
     static esp_err_t readiness_get(httpd_req_t* request);
+    static esp_err_t maintenance_post(httpd_req_t* request);
+    static esp_err_t hardware_verify_post(httpd_req_t* request);
+    static esp_err_t dry_run_post(httpd_req_t* request);
+    static esp_err_t valve_profile_post(httpd_req_t* request);
+    static esp_err_t bench_pulse_post(httpd_req_t* request);
+    static esp_err_t actuator_accept_post(httpd_req_t* request);
     static esp_err_t invalidate_post(httpd_req_t* request);
     static esp_err_t factory_reset_post(httpd_req_t* request);
+
     esp_err_t send_json(httpd_req_t* request, const std::string& body) const;
+    void refresh_control_state_from_store();
+    [[nodiscard]] bool maintenance_active() const;
 
     CommissioningNvsStore* store_{};
     hg::HardwareVerificationRecord* hardware_{};
@@ -42,8 +56,12 @@ private:
     hg::BootReadinessReport* readiness_{};
     hg::PhysicalOutputRuntime* physical_outputs_{};
     hg::SystemEventBus* bus_{};
+    hg::SystemModel* model_{};
+    HardwareBootstrap* hardware_runtime_{};
     std::mutex* control_state_mutex_{};
     homeguard::AccessControl* access_control_{};
+    bool maintenance_active_{};
+    std::uint8_t bench_valve_mask_{};
 };
 
 }  // namespace homeguard::idf
