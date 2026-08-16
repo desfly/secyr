1a0ae907b4b8483200f97ca09477131538f9b852
desfly <155248075+desfly@users.noreply.github.com>
2026-08-16 08:53:07 +0300

    Make W5500 initialization rollback safe

--- firmware/esp-idf/main/hg_w5500.hpp
+++ firmware/esp-idf/main/hg_w5500.hpp
@@ -1,6 +1,9 @@
 #pragma once
 
 #include "esp_eth.h"
+#include "esp_eth_mac.h"
+#include "esp_eth_netif_glue.h"
+#include "esp_eth_phy.h"
 #include "esp_netif.h"
 #include "esp_err.h"
 #include <cstdint>
@@ -22,6 +25,7 @@ public:
     esp_err_t initialize();
     esp_err_t start();
     esp_err_t stop();
+    esp_err_t deinitialize();
     const W5500Status& status() const noexcept;
 
 private:
@@ -38,7 +42,13 @@ private:
         void* data);
 
     esp_eth_handle_t eth_{nullptr};
+    esp_eth_mac_t* mac_{nullptr};
+    esp_eth_phy_t* phy_{nullptr};
+    esp_eth_netif_glue_handle_t glue_{nullptr};
     esp_netif_t* netif_{nullptr};
+    bool spi_bus_owned_{false};
+    bool eth_event_registered_{false};
+    bool ip_event_registered_{false};
     W5500Status status_{};
 };
 
