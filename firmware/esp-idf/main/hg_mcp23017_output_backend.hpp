6b13e50adf1ec66c444f50278a941c4b596694df
desfly <155248075+desfly@users.noreply.github.com>
2026-08-16 09:58:01 +0300

    firmware: add MCP23017 physical output backend

---
+++ firmware/esp-idf/main/hg_mcp23017_output_backend.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "hg_mcp23017.hpp"
+#include "homeguard/physical_output_runtime.hpp"
+
+#include <array>
+#include <cstdint>
+
+namespace homeguard::idf {
+
+class Mcp23017OutputBackend final : public hg::PhysicalOutputBackend {
+public:
+    void attach(Mcp23017* expander) noexcept;
+
+    bool configure_output(int channel, bool initial_level) override;
+    bool write_output(int channel, bool level) override;
+
+private:
+    static bool valid_channel(int channel) noexcept;
+    static std::uint8_t interlocked_value(
+        std::uint8_t current,
+        int channel,
+        bool level) noexcept;
+    bool commit(std::uint8_t value);
+
+    Mcp23017* expander_{};
+    std::array<bool, 8> configured_{};
+    std::uint8_t shadow_{};
+};
+
+}  // namespace homeguard::idf
