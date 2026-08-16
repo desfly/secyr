7cf163b5b15d78ca9017b61429e8ef629391fa8c
desfly <155248075+desfly@users.noreply.github.com>
2026-08-16 08:58:32 +0300

    Extend ESP-IDF mocks for lifecycle hardening

--- tests/esp-idf-mock/include/esp_netif.h
+++ tests/esp-idf-mock/include/esp_netif.h
@@ -12,6 +12,7 @@ inline esp_err_t esp_netif_attach(esp_netif_t*,void*){return ESP_OK;}
 inline esp_netif_t* esp_netif_create_default_wifi_ap(){static esp_netif_t n;return &n;}
 inline esp_netif_t* esp_netif_create_default_wifi_sta(){static esp_netif_t n;return &n;}
 inline void esp_netif_destroy_default_wifi(esp_netif_t*){}
+inline void esp_netif_destroy(esp_netif_t*){}
 inline esp_netif_t* esp_netif_get_handle_from_ifkey(const char*){static esp_netif_t n;return &n;}
 inline esp_err_t esp_netif_get_ip_info(esp_netif_t*, esp_netif_ip_info_t*){return ESP_OK;}
 
