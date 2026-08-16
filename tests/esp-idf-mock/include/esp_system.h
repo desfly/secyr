c06ebfa25808712b9b4819dacfab1203c7207038
desfly <155248075+desfly@users.noreply.github.com>
2026-08-16 16:17:38 +0300

    test: mock ESP reset reasons for triple-RST detector

--- tests/esp-idf-mock/include/esp_system.h
+++ tests/esp-idf-mock/include/esp_system.h
@@ -1,2 +1,29 @@
 #pragma once
+
+typedef enum {
+    ESP_RST_UNKNOWN = 0,
+    ESP_RST_POWERON,
+    ESP_RST_EXT,
+    ESP_RST_SW,
+    ESP_RST_PANIC,
+    ESP_RST_INT_WDT,
+    ESP_RST_TASK_WDT,
+    ESP_RST_WDT,
+    ESP_RST_DEEPSLEEP,
+    ESP_RST_BROWNOUT,
+    ESP_RST_SDIO,
+    ESP_RST_USB,
+    ESP_RST_JTAG,
+    ESP_RST_EFUSE,
+    ESP_RST_PWR_GLITCH,
+    ESP_RST_CPU_LOCKUP,
+} esp_reset_reason_t;
+
+inline esp_reset_reason_t& mock_reset_reason_storage() {
+    static esp_reset_reason_t reason = ESP_RST_POWERON;
+    return reason;
+}
+
+inline esp_reset_reason_t esp_reset_reason() { return mock_reset_reason_storage(); }
+inline void mock_set_reset_reason(esp_reset_reason_t reason) { mock_reset_reason_storage() = reason; }
 inline void esp_restart() {}
