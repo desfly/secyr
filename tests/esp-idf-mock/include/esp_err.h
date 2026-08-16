3dc335fb4c83b1f7b7fcf3ffc0ca958af6d206df
desfly <155248075+desfly@users.noreply.github.com>
2026-08-16 08:58:23 +0300

    Extend ESP-IDF mocks for lifecycle hardening

--- tests/esp-idf-mock/include/esp_err.h
+++ tests/esp-idf-mock/include/esp_err.h
@@ -9,6 +9,7 @@ constexpr esp_err_t ESP_ERR_INVALID_STATE = 0x103;
 constexpr esp_err_t ESP_ERR_INVALID_SIZE = 0x104;
 constexpr esp_err_t ESP_ERR_NOT_FOUND = 0x105;
 constexpr esp_err_t ESP_ERR_TIMEOUT = 0x107;
+constexpr esp_err_t ESP_ERR_INVALID_RESPONSE = 0x108;
 constexpr esp_err_t ESP_ERR_INVALID_CRC = 0x109;
 constexpr esp_err_t ESP_ERR_NVS_NOT_FOUND = 0x1102;
 constexpr esp_err_t ESP_ERR_NVS_NO_FREE_PAGES = 0x110d;
