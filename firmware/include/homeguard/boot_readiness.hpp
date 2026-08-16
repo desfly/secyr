d175e6c8bcd181edce96f6571215b22851422ebf
desfly <155248075+desfly@users.noreply.github.com>
2026-08-16 10:06:40 +0300

    firmware: expose actuator-test boot gate

--- firmware/include/homeguard/boot_readiness.hpp
+++ firmware/include/homeguard/boot_readiness.hpp
@@ -13,6 +13,7 @@ enum class BootReadinessStatus {
     BlockedHardwareVerification,
     BlockedCommissioningState,
     BlockedDryRunRequired,
+    BlockedActuatorTestRequired,
     ReadyForPhysicalOutputs,
 };
 
