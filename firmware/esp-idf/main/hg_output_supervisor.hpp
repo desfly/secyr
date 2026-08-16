54117c79ad75a453f480120fb262f41d9a0805b3
desfly <155248075+desfly@users.noreply.github.com>
2026-08-16 11:22:51 +0300

    firmware: decouple output supervisor from global readiness

--- firmware/esp-idf/main/hg_output_supervisor.hpp
+++ firmware/esp-idf/main/hg_output_supervisor.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-#include "homeguard/boot_readiness.hpp"
 #include "homeguard/physical_output_runtime.hpp"
 #include "homeguard/system_model.hpp"
 
@@ -12,8 +11,7 @@ class OutputSupervisor {
 public:
     esp_err_t start(
         hg::PhysicalOutputRuntime* runtime,
-        const hg::SystemModel* model,
-        const hg::BootReadinessReport* readiness);
+        const hg::SystemModel* model);
 
 private:
     static void task_entry(void* context);
@@ -21,7 +19,6 @@ private:
 
     hg::PhysicalOutputRuntime* runtime_{};
     const hg::SystemModel* model_{};
-    const hg::BootReadinessReport* readiness_{};
 };
 
 }  // namespace homeguard::idf
