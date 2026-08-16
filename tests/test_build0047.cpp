ddf5910a55846348f6c46fa243bdbaefbd7f7beb
desfly <155248075+desfly@users.noreply.github.com>
2026-08-16 10:12:24 +0300

    tests: require measured valve safety profile

--- tests/test_build0047.cpp
+++ tests/test_build0047.cpp
@@ -11,6 +11,13 @@ void test_build0047() {
 
     state.gpio_map_verified = true;
     state.active_polarity_verified = true;
+    CHECK(hg::validate_commissioning_state(state) == hg::CommissioningStateValidation::ValveSafetyUnverified);
+    CHECK(!hg::commissioning_state_allows_physical_outputs(state));
+
+    state.valve_limit_polarity_verified = true;
+    state.valve_limits_active_low = true;
+    state.cold_valve_travel_timeout_ms = 12345;
+    state.hot_valve_travel_timeout_ms = 14567;
     CHECK(hg::validate_commissioning_state(state) == hg::CommissioningStateValidation::Valid);
     CHECK(!hg::commissioning_state_allows_physical_outputs(state));
 
@@ -21,6 +28,11 @@ void test_build0047() {
 
     state.successful_actuator_tests = 1;
     CHECK(hg::commissioning_state_allows_physical_outputs(state));
+
+    auto no_timeout = state;
+    no_timeout.cold_valve_travel_timeout_ms = 0;
+    CHECK(hg::validate_commissioning_state(no_timeout) == hg::CommissioningStateValidation::ValveSafetyUnverified);
+    CHECK(!hg::commissioning_state_allows_physical_outputs(no_timeout));
 
     state.successful_actuator_tests = 2;
     CHECK(hg::validate_commissioning_state(state) == hg::CommissioningStateValidation::InvalidSequence);
@@ -34,5 +46,7 @@ void test_build0047() {
     const std::string json = hg::commissioning_state_json(state);
     CHECK(json.find("\"gpioMapVerified\":true") != std::string::npos);
     CHECK(json.find("\"outputArchitecture\":\"mcp23017_port_a\"") != std::string::npos);
+    CHECK(json.find("\"valveLimitPolarityVerified\":true") != std::string::npos);
+    CHECK(json.find("\"coldValveTravelTimeoutMs\":12345") != std::string::npos);
     CHECK(json.find("\"physicalOutputsAllowed\":true") != std::string::npos);
 }
