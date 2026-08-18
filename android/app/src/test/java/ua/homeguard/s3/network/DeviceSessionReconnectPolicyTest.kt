b8465b80b4df1b95974b55884aa93c65b8a2785c
desfly <155248075+desfly@users.noreply.github.com>
2026-08-18 17:02:50 +0300

    test(android): cover auto reconnect gating

--- android/app/src/test/java/ua/homeguard/s3/network/DeviceSessionReconnectPolicyTest.kt
+++ android/app/src/test/java/ua/homeguard/s3/network/DeviceSessionReconnectPolicyTest.kt
@@ -1,7 +1,11 @@
 package ua.homeguard.s3.network
 
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
 import org.junit.Test
+import ua.homeguard.s3.model.ControlPath
+import ua.homeguard.s3.model.DeviceEndpoint
 
 class DeviceSessionReconnectPolicyTest {
     @Test
@@ -17,5 +21,27 @@ class DeviceSessionReconnectPolicyTest {
     @Test
     fun negativeAttemptUsesFirstDelay() {
         assertEquals(2_000L, reconnectDelayMs(-1))
+    }
+
+    @Test
+    fun disabledAutoReconnectBlocksRetryForOtherwiseValidTarget() {
+        val endpoint = DeviceEndpoint(
+            deviceId = "HG-1",
+            apiBaseUrl = "http://192.168.1.20",
+            websocketUrl = "ws://192.168.1.20/ws",
+            path = ControlPath.LOCAL,
+        )
+
+        assertFalse(reconnectAllowed(false, endpoint, "token"))
+        assertTrue(reconnectAllowed(true, endpoint, "token"))
+    }
+
+    @Test
+    fun reconnectStillRequiresConnectableEndpointAndToken() {
+        val offline = DeviceEndpoint("HG-1", "", "", ControlPath.OFFLINE)
+        val local = DeviceEndpoint("HG-1", "http://192.168.1.20", "ws://192.168.1.20/ws", ControlPath.LOCAL)
+
+        assertFalse(reconnectAllowed(true, offline, "token"))
+        assertFalse(reconnectAllowed(true, local, ""))
     }
 }
