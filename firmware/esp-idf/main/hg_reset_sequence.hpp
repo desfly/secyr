59ce5848c1cecb619032a5cf805ded8bae177cd0
desfly <155248075+desfly@users.noreply.github.com>
2026-08-19 11:56:24 +0300

    Document final reset gesture colors

--- firmware/esp-idf/main/hg_reset_sequence.hpp
+++ firmware/esp-idf/main/hg_reset_sequence.hpp
@@ -5,7 +5,7 @@
 namespace homeguard::idf {
 
 // Starts the runtime factory-reset gesture on the dedicated service button:
-// hold until RED, release; repeat three times; successful erase -> WHITE 5 s -> reboot.
+// hold until WHITE, release; repeat three times; successful erase -> RED 5 s -> reboot.
 esp_err_t start_service_button_factory_reset();
 
 }  // namespace homeguard::idf
