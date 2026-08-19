45a95deb0710178ea340b1a10345cd4364665731
desfly <155248075+desfly@users.noreply.github.com>
2026-08-19 12:57:20 +0300

    Require authentication for hardware status

--- firmware/esp-idf/main/hg_infrastructure_http.hpp
+++ firmware/esp-idf/main/hg_infrastructure_http.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "homeguard/access_control.hpp"
 #include "esp_err.h"
 #include "esp_http_server.h"
 
@@ -11,11 +12,15 @@ class InfrastructureHttp {
 public:
     esp_err_t register_handlers(
         httpd_handle_t server,
-        HardwareBootstrap* hardware);
+        HardwareBootstrap* hardware,
+        homeguard::AccessControl* access_control);
 
 private:
     static esp_err_t status_get(httpd_req_t* request);
     static esp_err_t rgb_test_post(httpd_req_t* request);
+
+    HardwareBootstrap* hardware_{};
+    homeguard::AccessControl* access_control_{};
 };
 
 }  // namespace homeguard::idf
