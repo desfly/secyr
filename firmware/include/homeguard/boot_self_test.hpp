b4f124b7ce46a9d3e38eb24017af4db0102e7972
desfly <155248075+desfly@users.noreply.github.com>
2026-08-19 10:46:05 +0300

    Remove unused legacy boot self-test contract

--- firmware/include/homeguard/boot_self_test.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "homeguard/health_monitor.hpp"
-#include <array>
-#include <cstdint>
-namespace hg {
-struct SelfTestInputs { bool nvs{}; bool rtc{}; bool adc1{}; bool adc2{}; bool w5500{}; bool wifi{}; bool inputs{}; bool outputs{}; };
-class BootSelfTest { public: static HealthState run(const SelfTestInputs& inputs, HealthMonitor& health, uint64_t now_ms); };
-}
