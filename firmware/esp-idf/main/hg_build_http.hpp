b65574e4fbeb1da0ed69525dba3583fe00dd9278
desfly <155248075+desfly@users.noreply.github.com>
2026-08-19 12:57:03 +0300

    Require authentication for build metadata

--- firmware/esp-idf/main/hg_build_http.hpp
+++ firmware/esp-idf/main/hg_build_http.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "homeguard/access_control.hpp"
 #include "esp_err.h"
 #include "esp_http_server.h"
 
@@ -7,10 +8,12 @@ namespace homeguard::idf {
 
 class BuildHttp {
 public:
-    esp_err_t register_handlers(httpd_handle_t server);
+    esp_err_t register_handlers(httpd_handle_t server, homeguard::AccessControl* access_control);
 
 private:
     static esp_err_t build_get(httpd_req_t* request);
+
+    homeguard::AccessControl* access_control_{};
 };
 
 }  // namespace homeguard::idf
