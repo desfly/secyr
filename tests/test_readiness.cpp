37488a4b92380b5c7f0b8ca7934d6358b2bc107e
desfly <155248075+desfly@users.noreply.github.com>
2026-08-19 09:43:05 +0300

    Fold commissioning readiness checks into thematic test

--- tests/test_readiness.cpp
+++ tests/test_readiness.cpp
@@ -1,7 +1,10 @@
 #include "test_framework.hpp"
 #include "homeguard/boot_readiness.hpp"
+#include "homeguard/commissioning_state.hpp"
 #include "homeguard/hardware_verification.hpp"
 #include "homeguard/service_readiness.hpp"
+
+#include <string>
 
 namespace {
 hg::HardwareVerificationRecord valid_hardware() {
@@ -34,6 +37,34 @@ hg::CommissioningPersistentState valid_commissioning() {
     s.successful_actuator_tests = 1U;
     s.last_verified_at_ms = 1000U;
     return s;
+}
+
+void test_commissioning_state() {
+    hg::CommissioningPersistentState state{};
+    CHECK(hg::validate_commissioning_state(state) == hg::CommissioningStateValidation::VerificationIncomplete);
+    CHECK(!hg::commissioning_state_allows_physical_outputs(state));
+
+    state.gpio_map_verified = true;
+    state.active_polarity_verified = true;
+    CHECK(hg::validate_commissioning_state(state) == hg::CommissioningStateValidation::Valid);
+    CHECK(!hg::commissioning_state_allows_physical_outputs(state));
+
+    state.successful_dry_runs = 1;
+    state.last_verified_at_ms = 123456;
+    CHECK(hg::commissioning_state_allows_physical_outputs(state));
+
+    state.successful_actuator_tests = 2;
+    CHECK(hg::validate_commissioning_state(state) == hg::CommissioningStateValidation::InvalidSequence);
+    CHECK(!hg::commissioning_state_allows_physical_outputs(state));
+
+    state.successful_actuator_tests = 1;
+    state.schema_version = 99;
+    CHECK(hg::validate_commissioning_state(state) == hg::CommissioningStateValidation::InvalidSchema);
+
+    state.schema_version = hg::CommissioningPersistentState::kSchemaVersion;
+    const std::string json = hg::commissioning_state_json(state);
+    CHECK(json.find("\"gpioMapVerified\":true") != std::string::npos);
+    CHECK(json.find("\"physicalOutputsAllowed\":true") != std::string::npos);
 }
 
 void test_hardware_verification() {
@@ -142,6 +173,7 @@ void test_service_readiness() {
 } // namespace
 
 void test_readiness() {
+    test_commissioning_state();
     test_hardware_verification();
     test_boot_readiness();
     test_service_readiness();
