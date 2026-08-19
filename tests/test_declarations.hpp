fd9bd56760785e45e2610ada3b033434208a2045
desfly <155248075+desfly@users.noreply.github.com>
2026-08-19 09:49:26 +0300

    Remove legacy controller test declarations

--- tests/test_declarations.hpp
+++ tests/test_declarations.hpp
@@ -2,7 +2,6 @@
 void test_zone();
 void test_pressure();
 void test_reliability();
-void test_controller();
 void test_connectivity();
 void test_provisioning();
 void test_reset_sequence();
@@ -12,7 +11,6 @@ void test_access_permissions();
 void test_output_safety();
 void test_readiness();
 void test_build0009();
-void test_build0013();
 void test_build0032();
 void test_build0038();
 void test_build0045();
