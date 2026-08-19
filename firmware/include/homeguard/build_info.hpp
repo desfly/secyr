833ec0c27932be6d197eb7574fb6ccadec3bcf52
desfly <155248075+desfly@users.noreply.github.com>
2026-08-19 07:40:16 +0300

    Remove stale unused build_info header

--- firmware/include/homeguard/build_info.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <string_view>
-namespace hg::build {
-inline constexpr std::string_view number = "0039";
-inline constexpr std::string_view version = "0.0.39";
-inline constexpr std::string_view label = "HomeGuard-S3 Build-0039";
-}
