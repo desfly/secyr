ec499daf9d52b23991ed49526d6fbf587e9a5438
desfly <155248075+desfly@users.noreply.github.com>
2026-08-19 13:00:07 +0300

    Keep access HTTP interface build-consistent

--- firmware/esp-idf/main/hg_access_http.hpp
+++ firmware/esp-idf/main/hg_access_http.hpp
@@ -12,7 +12,7 @@ public:
     esp_err_t register_handlers(httpd_handle_t server,
                                 AccessControl* access,
                                 AccessNvsStore* store,
-                                bool* bootstrap_allowed);
+                                bool bootstrap_allowed);
 
 private:
     static esp_err_t state_get(httpd_req_t* request);
@@ -22,11 +22,10 @@ private:
     esp_err_t handle_state(httpd_req_t* request);
     esp_err_t handle_users(httpd_req_t* request);
     esp_err_t handle_login(httpd_req_t* request);
-    [[nodiscard]] bool setup_required() const;
 
     AccessControl* access_{};
     AccessNvsStore* store_{};
-    bool* bootstrap_allowed_{};
+    bool bootstrap_allowed_{};
 };
 
 }  // namespace homeguard::idf
