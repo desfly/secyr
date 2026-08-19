730a3d1ac7f2eeaf2a0a9724d0d052c59d03a92c
desfly <155248075+desfly@users.noreply.github.com>
2026-08-19 07:07:20 +0300

    Expire and consume telemetry session tokens

--- firmware/esp-idf/components/websocket_telemetry/include/websocket_telemetry.hpp
+++ firmware/esp-idf/components/websocket_telemetry/include/websocket_telemetry.hpp
@@ -4,6 +4,7 @@
 #include "esp_http_server.h"
 #include <array>
 #include <cstddef>
+#include <cstdint>
 #include <mutex>
 #include <string>
 #include <string_view>
@@ -20,13 +21,14 @@ private:
     static int websocket_entry(httpd_req_t* request);
     static void broadcast_work_entry(void* context);
     int websocket(httpd_req_t* request);
-    bool authorize(httpd_req_t* request) const;
+    bool authorize(httpd_req_t* request);
     void add_client(int fd);
     void remove_client(int fd);
     void run_broadcast(BroadcastWork& work);
     void* server_{};
     hg::BearerTokenVerifier token_{};
     std::array<hg::BearerTokenVerifier, 4> session_tokens_{};
+    std::array<std::int64_t, 4> session_token_issued_us_{};
     std::size_t next_session_token_{};
     std::array<int, 4> clients_{{-1, -1, -1, -1}};
     mutable std::mutex mutex_{};
