4ca50c1fe21dd5e0e1dff26e3040fc88fab44f5a
desfly <155248075+desfly@users.noreply.github.com>
2026-08-19 12:55:28 +0300

    Require access control for LAN API

--- firmware/esp-idf/main/hg_lan_http.hpp
+++ firmware/esp-idf/main/hg_lan_http.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "homeguard/access_control.hpp"
 #include "esp_err.h"
 #include "esp_http_server.h"
 
@@ -9,13 +10,16 @@ namespace homeguard::idf {
 
 class LanHttp {
 public:
-    esp_err_t register_handlers(httpd_handle_t server);
+    esp_err_t register_handlers(httpd_handle_t server, homeguard::AccessControl* access_control);
 
 private:
     static esp_err_t devices_get(httpd_req_t* request);
     static esp_err_t scan_get(httpd_req_t* request);
 
+    [[nodiscard]] bool authenticated_request(httpd_req_t* request) const;
     std::string devices_json(bool active_scan) const;
+
+    homeguard::AccessControl* access_control_{};
 };
 
 }  // namespace homeguard::idf
