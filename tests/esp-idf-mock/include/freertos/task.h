1c0f485c04474f3fe66620fc3374a3a5f5c1724f
desfly <155248075+desfly@users.noreply.github.com>
2026-08-19 11:58:19 +0300

    Mock FreeRTOS tick clock for reset gesture

--- tests/esp-idf-mock/include/freertos/task.h
+++ tests/esp-idf-mock/include/freertos/task.h
@@ -2,5 +2,6 @@
 #include "freertos/FreeRTOS.h"
 using TaskFunction_t = void(*)(void*);
 inline int xTaskCreate(TaskFunction_t, const char*, unsigned, void*, unsigned, void*) { return pdPASS; }
+inline TickType_t xTaskGetTickCount() { static TickType_t tick = 0; return ++tick; }
 inline void vTaskDelay(TickType_t) {}
 inline void vTaskDelete(void*) {}
