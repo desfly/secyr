cdcaeb9a33e5149d014b7eed6eaacf9282adf041
desfly <155248075+desfly@users.noreply.github.com>
2026-08-21 02:14:38 +0300

    Match ESP-IDF IPv4 mock shape

--- tests/esp-idf-mock/include/esp_netif_ip_addr.h
+++ tests/esp-idf-mock/include/esp_netif_ip_addr.h
@@ -1,5 +1,6 @@
 #pragma once
-struct esp_ip4_addr_t {};
+#include <cstdint>
+struct esp_ip4_addr_t { std::uint32_t addr{}; };
 struct esp_netif_ip_info_t { esp_ip4_addr_t ip; };
 struct ip_event_got_ip_t { esp_netif_ip_info_t ip_info; };
-inline const char* esp_ip4addr_ntoa(const esp_ip4_addr_t*,char* out,int){out[0]='0';out[1]=0;return out;}
+inline const char* esp_ip4addr_ntoa(const esp_ip4_addr_t*,char* out,int length){if(length>0){out[0]='0';if(length>1)out[1]=0;}return out;}
