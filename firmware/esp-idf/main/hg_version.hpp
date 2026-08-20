f56bb187207869c040e080b92ab6c012bafbefaa
desfly <155248075+desfly@users.noreply.github.com>
2026-08-21 02:13:53 +0300

    Bump HomeGuard build for Wi-Fi stability rewrite

--- firmware/esp-idf/main/hg_version.hpp
+++ firmware/esp-idf/main/hg_version.hpp
@@ -1,8 +1,8 @@
 #pragma once
 
 #define HG_PROJECT_NAME "HomeGuard-S3"
-#define HG_BUILD_NUMBER "0026"
-#define HG_FIRMWARE_VERSION "0.26.0-test"
+#define HG_BUILD_NUMBER "0027"
+#define HG_FIRMWARE_VERSION "0.27.0-test"
 #define HG_BOARD_NAME "HW-678 V0.0.0"
 #define HG_MODULE_NAME "ESP32-S3-WROOM-1-N16R8"
 #define HG_ESP_IDF_REQUIRED "5.4.4"
