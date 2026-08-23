620105fcb698b02f2c5329b950f7752603cf4aee
desfly <155248075+desfly@users.noreply.github.com>
2026-08-23 12:25:33 +0300

    Advertise setup captive portal to Android

--- firmware/esp-idf/main/hg_setup_ap_guard.cpp
+++ firmware/esp-idf/main/hg_setup_ap_guard.cpp
@@ -2,9 +2,12 @@
 
 #include "esp_err.h"
 #include "esp_log.h"
+#include "esp_netif.h"
 #include "esp_wifi.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+
+#include <cstring>
 
 namespace homeguard::idf {
 namespace {
@@ -14,16 +17,52 @@ constexpr TickType_t kPollDelay = pdMS_TO_TICKS(100);
 constexpr TickType_t kPostBootstrapResponseDelay = pdMS_TO_TICKS(750);
 constexpr unsigned kTaskStackBytes = 2048;
 constexpr unsigned kTaskPriority = 3;
+static char kCaptivePortalUri[] = "http://192.168.4.1";
+
+bool configure_setup_captive_portal()
+{
+#ifdef CONFIG_ESP_ENABLE_DHCP_CAPTIVEPORTAL
+    auto* ap_netif = esp_netif_get_handle_from_ifkey("WIFI_AP_DEF");
+    if (ap_netif == nullptr) return false;
+
+    const auto stop_error = esp_netif_dhcps_stop(ap_netif);
+    if (stop_error != ESP_OK && stop_error != ESP_ERR_ESP_NETIF_DHCP_ALREADY_STOPPED) {
+        return false;
+    }
+
+    const auto option_error = esp_netif_dhcps_option(ap_netif,
+                                                     ESP_NETIF_OP_SET,
+                                                     ESP_NETIF_CAPTIVEPORTAL_URI,
+                                                     kCaptivePortalUri,
+                                                     std::strlen(kCaptivePortalUri));
+    if (option_error != ESP_OK) {
+        (void)esp_netif_dhcps_start(ap_netif);
+        return false;
+    }
+
+    const auto start_error = esp_netif_dhcps_start(ap_netif);
+    if (start_error != ESP_OK && start_error != ESP_ERR_ESP_NETIF_DHCP_ALREADY_STARTED) {
+        return false;
+    }
+
+    ESP_LOGI(kTag, "Android/iOS captive portal advertised at %s", kCaptivePortalUri);
+#endif
+    return true;
+}
 
 void setup_ap_guard_task(void*)
 {
     bool bootstrap_seen = false;
     bool response_delay_applied = false;
+    bool captive_portal_configured = false;
 
     for (;;) {
         if (access_runtime::bootstrap_allowed()) {
             bootstrap_seen = true;
             response_delay_applied = false;
+            if (!captive_portal_configured) {
+                captive_portal_configured = configure_setup_captive_portal();
+            }
             vTaskDelay(kPollDelay);
             continue;
         }
