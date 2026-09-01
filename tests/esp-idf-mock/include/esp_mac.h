b92773dbde6ba38308ed9e89e7f6868ad8226114
desfly <155248075+desfly@users.noreply.github.com>
2026-09-01 21:29:55 +0300

    test: provide deterministic mock Ethernet MAC

--- tests/esp-idf-mock/include/esp_mac.h
+++ tests/esp-idf-mock/include/esp_mac.h
@@ -14,7 +14,19 @@ typedef enum {
     ESP_MAC_ETH
 } esp_mac_type_t;
 
-esp_err_t esp_read_mac(uint8_t* mac, esp_mac_type_t type);
+static inline esp_err_t esp_read_mac(uint8_t* mac, esp_mac_type_t type)
+{
+    if (mac == nullptr) {
+        return ESP_ERR_INVALID_ARG;
+    }
+    mac[0] = 0x02;
+    mac[1] = 0x00;
+    mac[2] = 0x00;
+    mac[3] = 0x00;
+    mac[4] = 0x00;
+    mac[5] = static_cast<uint8_t>(type);
+    return ESP_OK;
+}
 
 #ifdef __cplusplus
 }
