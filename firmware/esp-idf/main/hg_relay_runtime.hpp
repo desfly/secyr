6008c52d8c1d9eac889d1cafaf68bf30e3b269f3
desfly <155248075+desfly@users.noreply.github.com>
2026-09-09 10:27:16 +0300

    relay: move runtime to direct GPIO outputs

--- firmware/esp-idf/main/hg_relay_runtime.hpp
+++ firmware/esp-idf/main/hg_relay_runtime.hpp
@@ -8,7 +8,6 @@
 
 namespace homeguard::idf {
 
-class Mcp23017;
 class ZoneMonitor;
 
 struct RelayRuntimeState {
@@ -16,34 +15,37 @@ struct RelayRuntimeState {
     bool light_manual{};
     bool light_automatic{};
     bool lock_active{};
+    bool valve1_active{};
+    bool valve2_active{};
     std::uint32_t lock_remaining_ms{};
 };
 
 class RelayRuntime {
 public:
-    static constexpr std::uint8_t kLightMask = 1U << 0U;  // MCP23017 GPA0
-    static constexpr std::uint8_t kLockMask = 1U << 6U;   // MCP23017 GPA6
     static constexpr std::uint32_t kLightCycleMs = 60'000U;
     static constexpr std::uint32_t kLockPulseMs = 5'000U;
 
-    esp_err_t start(Mcp23017* expander, ZoneMonitor* zones);
+    esp_err_t start(ZoneMonitor* zones);
     bool set_manual_light(bool active);
     bool request_lock_pulse();
+    bool set_valve(std::uint8_t index, bool active);
     RelayRuntimeState state();
 
 private:
     static void task_entry(void* context);
     void run();
-    bool write_state_locked(bool light, bool lock);
+    bool write_state_locked(bool light, bool lock, bool valve1, bool valve2);
+    bool force_safe_locked();
     static std::uint64_t now_ms();
 
-    Mcp23017* expander_{nullptr};
     ZoneMonitor* zones_{nullptr};
     SemaphoreHandle_t mutex_{nullptr};
     bool light_active_{false};
     bool manual_light_{false};
     bool automatic_light_{false};
     bool lock_active_{false};
+    bool valve1_active_{false};
+    bool valve2_active_{false};
     std::uint64_t light_deadline_ms_{0};
     std::uint64_t lock_deadline_ms_{0};
 };
