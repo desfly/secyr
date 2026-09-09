8b43b9fa016ce8039687e08f50734554a084f42e
desfly <155248075+desfly@users.noreply.github.com>
2026-09-09 09:13:41 +0300

    relay: add state and light HTTP routes

--- firmware/esp-idf/main/hg_relay_http.hpp
+++ firmware/esp-idf/main/hg_relay_http.hpp
@@ -13,6 +13,8 @@ public:
     esp_err_t register_handlers(httpd_handle_t server, RelayRuntime* relays, homeguard::AccessControl* access_control);
 
 private:
+    static esp_err_t state_get(httpd_req_t* request);
+    static esp_err_t light_post(httpd_req_t* request);
     static esp_err_t lock_post(httpd_req_t* request);
 
     RelayRuntime* relays_{nullptr};
