aaac33d3b27900edb3c1cb4c1661898f61a97daa
desfly <155248075+desfly@users.noreply.github.com>
2026-09-09 09:19:55 +0300

    android: model explicit unavailable relay state

--- android/app/src/main/java/ua/homeguard/s3/model/RelayControlState.kt
+++ android/app/src/main/java/ua/homeguard/s3/model/RelayControlState.kt
@@ -1,10 +1,14 @@
 package ua.homeguard.s3.model
 
 data class RelayControlState(
-    val available: Boolean = false,
+    val available: Boolean = true,
     val lightActive: Boolean = false,
     val lightManual: Boolean = false,
     val lightAutomatic: Boolean = false,
     val lockActive: Boolean = false,
     val lockRemainingMs: Int = 0,
-)
+) {
+    companion object {
+        fun unavailable() = RelayControlState(available = false)
+    }
+}
