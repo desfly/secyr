4acf37d0d40648417e487c69f5571f699f7a1913
desfly <155248075+desfly@users.noreply.github.com>
2026-09-09 09:16:24 +0300

    android: add relay control state model

---
+++ android/app/src/main/java/ua/homeguard/s3/model/RelayControlState.kt
@@ -0,0 +1,9 @@
+package ua.homeguard.s3.model
+
+data class RelayControlState(
+    val lightActive: Boolean = false,
+    val lightManual: Boolean = false,
+    val lightAutomatic: Boolean = false,
+    val lockActive: Boolean = false,
+    val lockRemainingMs: Int = 0,
+)
