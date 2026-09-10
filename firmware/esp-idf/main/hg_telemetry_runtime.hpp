cc9b06d22130a595872eb22dc5de9f4d908b621c
desfly <155248075+desfly@users.noreply.github.com>
2026-09-10 15:30:41 +0300

    firmware: route telemetry to BLE sink

--- firmware/esp-idf/main/hg_telemetry_runtime.hpp
+++ firmware/esp-idf/main/hg_telemetry_runtime.hpp
@@ -12,12 +12,14 @@ class WebsocketTelemetry;
 namespace homeguard::idf {
 
 class HardwareBootstrap;
+class BleTransport;
 
 class TelemetryRuntime {
 public:
     esp_err_t start(
         HardwareBootstrap* hardware,
         WebsocketTelemetry* websocket,
+        BleTransport* ble,
         const hg::SystemModel* system_model);
 
 private:
@@ -26,6 +28,7 @@ private:
 
     HardwareBootstrap* hardware_{nullptr};
     WebsocketTelemetry* websocket_{nullptr};
+    BleTransport* ble_{nullptr};
     const hg::SystemModel* system_model_{nullptr};
     hg::TelemetryBuilder builder_{};
     hg::HealthMonitor health_{};
