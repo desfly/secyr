2efee700494d8b086d2c1409c3417a67a3ce752b
desfly <155248075+desfly@users.noreply.github.com>
2026-09-10 15:31:30 +0300

    firmware: own BLE transport inside telemetry runtime

--- firmware/esp-idf/main/hg_telemetry_runtime.hpp
+++ firmware/esp-idf/main/hg_telemetry_runtime.hpp
@@ -1,26 +1,19 @@
 #pragma once
 
 #include "homeguard/telemetry.hpp"
+#include "hg_ble_transport.hpp"
 #include "esp_err.h"
 
-namespace hg {
-class SystemModel;
-}
-
+namespace hg { class SystemModel; }
 class WebsocketTelemetry;
 
 namespace homeguard::idf {
-
 class HardwareBootstrap;
-class BleTransport;
 
 class TelemetryRuntime {
 public:
-    esp_err_t start(
-        HardwareBootstrap* hardware,
-        WebsocketTelemetry* websocket,
-        BleTransport* ble,
-        const hg::SystemModel* system_model);
+    esp_err_t start(HardwareBootstrap* hardware, WebsocketTelemetry* websocket, const hg::SystemModel* system_model);
+    BleTransport& ble() { return ble_; }
 
 private:
     static void task_entry(void* context);
@@ -28,8 +21,8 @@ private:
 
     HardwareBootstrap* hardware_{nullptr};
     WebsocketTelemetry* websocket_{nullptr};
-    BleTransport* ble_{nullptr};
     const hg::SystemModel* system_model_{nullptr};
+    BleTransport ble_{};
     hg::TelemetryBuilder builder_{};
     hg::HealthMonitor health_{};
 };
