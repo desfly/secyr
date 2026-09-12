f7bd737b062f7afada600bcc0ea7c63bb091683d
desfly <155248075+desfly@users.noreply.github.com>
2026-09-12 21:57:29 +0300

    fix(light): wire zone-triggered one-minute light runtime

--- firmware/esp-idf/main/hg_telemetry_runtime.hpp
+++ firmware/esp-idf/main/hg_telemetry_runtime.hpp
@@ -5,6 +5,8 @@
 
 namespace hg {
 class SystemModel;
+class PhysicalOutputRuntime;
+struct BootReadinessReport;
 }
 
 class WebsocketTelemetry;
@@ -19,19 +21,29 @@ public:
     esp_err_t start(
         HardwareBootstrap* hardware,
         WebsocketTelemetry* websocket,
-        const hg::SystemModel* system_model,
+        hg::SystemModel* system_model,
+        hg::PhysicalOutputRuntime* physical_outputs,
+        hg::BootReadinessReport* readiness,
         BleTransport* ble_transport = nullptr);
 
 private:
     static void task_entry(void* context);
     void run();
+    void update_zone_light(const std::array<hg::ZoneState, 8>& zones, std::uint64_t now_ms);
+    bool set_light_output(bool active, std::uint64_t now_ms);
 
     HardwareBootstrap* hardware_{nullptr};
     WebsocketTelemetry* websocket_{nullptr};
-    const hg::SystemModel* system_model_{nullptr};
+    hg::SystemModel* system_model_{nullptr};
+    hg::PhysicalOutputRuntime* physical_outputs_{nullptr};
+    hg::BootReadinessReport* readiness_{nullptr};
     BleTransport* ble_transport_{nullptr};
     hg::TelemetryBuilder builder_{};
     hg::HealthMonitor health_{};
+
+    bool light_cycle_active_{false};
+    bool light_restore_active_{false};
+    std::uint64_t light_cycle_deadline_ms_{0};
 };
 
 }  // namespace homeguard::idf
