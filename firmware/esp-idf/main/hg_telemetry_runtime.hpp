765f9bf050d74978f5a98d4755d4d381f4434e5c
desfly <155248075+desfly@users.noreply.github.com>
2026-09-12 19:02:55 +0300

    fix(zones): restore proven telemetry model contract

--- firmware/esp-idf/main/hg_telemetry_runtime.hpp
+++ firmware/esp-idf/main/hg_telemetry_runtime.hpp
@@ -19,7 +19,7 @@ public:
     esp_err_t start(
         HardwareBootstrap* hardware,
         WebsocketTelemetry* websocket,
-        hg::SystemModel* system_model,
+        const hg::SystemModel* system_model,
         BleTransport* ble_transport = nullptr);
 
 private:
@@ -28,7 +28,7 @@ private:
 
     HardwareBootstrap* hardware_{nullptr};
     WebsocketTelemetry* websocket_{nullptr};
-    hg::SystemModel* system_model_{nullptr};
+    const hg::SystemModel* system_model_{nullptr};
     BleTransport* ble_transport_{nullptr};
     hg::TelemetryBuilder builder_{};
     hg::HealthMonitor health_{};
