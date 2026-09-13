2263d4542e20411cc18c5cec1f01dba561d5be64
desfly <155248075+desfly@users.noreply.github.com>
2026-09-13 22:01:59 +0300

    test(mock): model FreeRTOS task handles

--- tests/esp-idf-mock/include/freertos/task.h
+++ tests/esp-idf-mock/include/freertos/task.h
@@ -1,7 +1,8 @@
 #pragma once
 #include "freertos/FreeRTOS.h"
 using TaskFunction_t = void(*)(void*);
-inline int xTaskCreate(TaskFunction_t, const char*, unsigned, void*, unsigned, void*) { return pdPASS; }
+using TaskHandle_t = void*;
+inline int xTaskCreate(TaskFunction_t, const char*, unsigned, void*, unsigned, TaskHandle_t*) { return pdPASS; }
 inline TickType_t xTaskGetTickCount() { static TickType_t tick = 0; return ++tick; }
 inline void vTaskDelay(TickType_t) {}
-inline void vTaskDelete(void*) {}
+inline void vTaskDelete(TaskHandle_t) {}
